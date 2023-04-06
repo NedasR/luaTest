bca899407771e1a5c152e239cf160c7e5675d7a0
Antonio Ganea <antonio.ganea@yahoo.com>
2023-04-06 22:17:00 +0300

    Moved utility code to utility.lua

--- main.lua
+++ main.lua
@@ -1,14 +1,4 @@
--- Removes all elements from the given list if they match the criteria specified by the predicate function
-function removeIf(list, predicate)
-  local i = 1
-  while i <= #list do
-    if predicate(list[i]) then
-      table.remove(list, i)
-    else
-      i = i + 1
-    end
-  end
-end
+require("utility")
 
 
 local rot = 0
