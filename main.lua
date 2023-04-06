34e9c88509cd5a2dc75014e4112f14f0cb3aa2d9
Antonio Ganea <antonio.ganea@yahoo.com>
2023-04-06 22:01:17 +0300

    Fixed indentation for the dummy love.draw() function

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
 function love.draw()
-    love.graphics.print("Hello World!", 300, 300)
-    love.graphics.print("tursty!", 300, 356)
-    love.graphics.print("Hello World!", 324, 655)
-    love.graphics.print("Hello World!", 540, 400)
-  end
+  love.graphics.print("Hello World!", 300, 300)
+  love.graphics.print("tursty!", 300, 356)
+  love.graphics.print("Hello World!", 324, 655)
+  love.graphics.print("Hello World!", 540, 400)
+end
 local rot = 0
  
 local x = 150
